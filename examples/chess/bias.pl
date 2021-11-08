85b9e1fc88d127a7f4a3d2e73615ca961105d8f5
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2021-11-08 16:43:22 -0500

    Redesign ILP problem formulation

--- examples/chess/bias.pl
+++ examples/chess/bias.pl
@@ -2,6 +2,11 @@ max_clauses(4).
 max_vars(4).
 max_body(3).
 
-head_pred(f,6).
+head_pred(f,3).
+
+body_pred(contents,3).
+body_pred(piece,1).
+body_pred(side,1).
+body_pred(square,2).
 
 allow_singletons.
--- examples/chess/bk.pl
+++ examples/chess/bk.pl
@@ -1,92 +1,51 @@
-legal_move(Side,Piece,Place,NewPlace,Pos1):-
-    contents(Side,Piece,Place,Pos1),
-    piece_move(Side,Piece,Place,NewPlace,Pos1),
-    do_move(Side,Piece,Place,NewPlace,Pos1,Pos2),
-    \+ in_check(Side,_,_,_,Pos2).
+cons(A,B,C):-
+    append([A],B,C).
+tail([_|T],T).
+head([H|_],H).
+empty([]).
 
-in_check(Side,KPlace,OPiece,OPlace,Pos):-
-    contents(Side,king,KPlace,Pos),
-    contents(OSide,OPiece,OPlace,Pos),
-    other_side(Side,OSide),
-    piece_move(OSide,OPiece,OPlace,KPlace,Pos).
+element([X|_],X):-!.
+element([_|T],X):-
+    element(T,X).
 
-check_mate(Side,Place,Pos):-
-    contents(Side,king,Place,Pos),
-    in_check(Side,Place,_,_,Pos),
-    \+ legal_move(Side,king,Place,_,Pos).
+is_list([]).
+is_list([_|_]).
 
-make_move(Side,Piece,Place,NewPlace,Pos1,Pos2):-
-    legal_move(Side,Piece,Place,NewPlace,Pos1),
-    other_side(Side,OSide),
-    \+ in_check(OSide,_,_,_,Pos1),
-    do_move(Side,Piece,Place,NewPlace,Pos1,Pos2).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-other_side(white,black).
-other_side(black,white).
+square(1,1). square(1,2). square(1,3). square(1,4). square(1,5). square(1,6). square(1,7). square(1,8).
+square(2,1). square(2,2). square(2,3). square(2,4). square(2,5). square(2,6). square(2,7). square(2,8).
+square(3,1). square(3,2). square(3,3). square(3,4). square(3,5). square(3,6). square(3,7). square(3,8).
+square(4,1). square(4,2). square(4,3). square(4,4). square(4,5). square(4,6). square(4,7). square(4,8).
+square(5,1). square(5,2). square(5,3). square(5,4). square(5,5). square(5,6). square(5,7). square(5,8).
+square(6,1). square(6,2). square(6,3). square(6,4). square(6,5). square(6,6). square(6,7). square(6,8).
+square(7,1). square(7,2). square(7,3). square(7,4). square(7,5). square(7,6). square(7,7). square(7,8).
+square(8,1). square(8,2). square(8,3). square(8,4). square(8,5). square(8,6). square(8,7). square(8,8).
 
-sliding_piece(Piece,Place,Pos):-
-    contents(Side,Piece,Place,Pos),
-    member(Piece,[queen,bishop,rook]).
+side(white).
+side(black).
 
-do_move(Side,Piece,Place,NewPlace,Pos1,Pos2) :-
-    current_state(State),
-    create_new_state(Pos1,State,Pos2,NState),
-    retract_if_there(contents(_,_,NewPlace,Pos2)),
-    retract(contents(Side,Piece,Place),Pos2),
-    asserta(contents(Side,Piece,NewPlace),Pos2),
-    !,
-    restore_if_redo(State).
+piece(Piece) :-
+    member(Piece, [pawn, knight, bishop, rook, queen, king]).
 
-current_state(State) :-
-    description_pred(Descript),
-    findall(Descript,State).
+contents(Side,Piece,square(X,Y)) :-
+    side(Side),
+    piece(Piece),
+    square(X,Y).
 
-restore_state(State) :-
-    description_pred(Descript),
-    retractall(Descript),
-    asserta_all(State).
+move(Side,Piece,square(X,Y),square(NewX,NewY)) :-
+    side(Side),
+    piece(Piece),
+    square(X,Y),
+    square(NewX,NewY).
 
-restore_if_redo(_).
-restore_if_redo(State) :-
-    restore_state(State),
-    !,
-    fail.
+% TODO: design a "state" property
 
-create_new_state(Pos,State,NPos,NState) :-
-    new_pos(NPos),
-    replace(Pos,NPos,State,NState),
-    asserta_all(NState).
+% legal move is one where piece of move color exists at move location
+% TODO: turn this into an actual legal_move property calculator?
+% if I have this working correctly, I don't need to pass in all the legal moves in the target relation
+legal_move(Side,Piece,square(X,Y),square(NewX,NewY)) :-
+    move(Side,Piece,square(X,Y),square(NewX,NewY)),
+    contents(Side,Piece,square(X,Y)).
 
-feature(contents(_,_,square(_,_),_)).
-feature(sliding_piece(_,square(_,_),_)).
-feature(check_mate(_,square(_,_),_)).
-feature(stale(_,_,square(_,_),_)).
-feature(stale(_,_),_).
-feature(legal_move(_,_,square(_,_),square(_,_),_)).
-feature(in_check(_,square(_,_),_,square(_,_),_)).
-
-description_pred(contents/4).
-domain(side,[white,black]).
-domain(piece,[pawn,knight,bishop,rook,queen,king]).
-domain(place,[square(1,1),square(1,2),...,square(8,8)]).
-type_arg(white,side) :- !.
-type_arg(black,side) :- !.
-type_arg(square(_,_),place) :- !.
-type_arg(_,piece).
-
-contents(black, king, square(5,7), pos1).
-contents(black, rook, square(3,7), pos1).
-contents(white, king, square(1,1), pos1).
-contents(white, knight, square(6,4), pos1).
-
-legal_move(white, king, square(1,1), square(2,1), pos1).
-legal_move(white, king, square(1,1), square(2,2), pos1).
-legal_move(white, king, square(1,1), square(1,2), pos1).
-legal_move(white, knight, square(6,4), square(4,5), pos1).
-legal_move(white, knight, square(6,4), square(8,5), pos1).
-legal_move(white, knight, square(6,4), square(5,6), pos1).
-legal_move(white, knight, square(6,4), square(7,6), pos1).
-legal_move(white, knight, square(6,4), square(4,3), pos1).
-legal_move(white, knight, square(6,4), square(8,3), pos1).
-legal_move(white, knight, square(6,4), square(5,2), pos1).
-legal_move(white, knight, square(6,4), square(7,2), pos1).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
