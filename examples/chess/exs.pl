e242843dfa65933e02856de7637cd3f92fd39266
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-01-12 15:56:33 -0500

    Add rules for learning fork

--- examples/chess/exs.pl
+++ examples/chess/exs.pl
@@ -1,18 +1,18 @@
-%pos(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 1, 1), contents(white, knight, 6, 4)], 6, 4, 4, 5)).
-%pos(f([contents(black, king, 1, 8), contents(black, queen, 3, 8), contents(white, king, 5, 1), contents(white, knight, 1, 4)], 1, 4, 2, 6)).
-%pos(f([contents(black, king, 1, 8), contents(black, queen, 4, 7), contents(white, king, 5, 1), contents(white, knight, 4, 5)], 4, 5, 3, 7)).
-%pos(f([contents(black, king, 7, 5), contents(black, queen, 6, 6), contents(white, king, 2, 4), contents(white, knight, 3, 5)], 3, 5, 5, 4)).
-%neg(f([contents(black, king, 1, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], 3, 4, 2, 6)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
-%neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+pos(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 1, 1), contents(white, knight, 6, 4)], 6, 4, 4, 5)).
+pos(f([contents(black, king, 1, 8), contents(black, queen, 3, 8), contents(white, king, 5, 1), contents(white, knight, 1, 4)], 1, 4, 2, 6)).
+pos(f([contents(black, king, 1, 8), contents(black, queen, 4, 7), contents(white, king, 5, 1), contents(white, knight, 4, 5)], 4, 5, 3, 7)).
+pos(f([contents(black, king, 7, 5), contents(black, queen, 6, 6), contents(white, king, 2, 4), contents(white, knight, 3, 5)], 3, 5, 5, 4)).
+neg(f([contents(black, king, 1, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], 3, 4, 2, 6)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], 5, 1, 4, 2)).
 
-pos(f([contents(white, knight, 3, 1), contents(white, king, 5, 1), contents(black, rook, 1, 8), contents(black, king, 5, 8)], 1, 8, 1, 1)).
-pos(f([contents(white, king, 5, 2), contents(white, queen, 5, 4), contents(black, knight, 3, 5), contents(black, king, 2, 6)], 5, 4, 4, 4)).
+%pos(f([contents(white, knight, 3, 1), contents(white, king, 5, 1), contents(black, rook, 1, 8), contents(black, king, 5, 8)], 1, 8, 1, 1)).
+%pos(f([contents(white, king, 5, 2), contents(white, queen, 5, 4), contents(black, knight, 3, 5), contents(black, king, 2, 6)], 5, 4, 4, 4)).
