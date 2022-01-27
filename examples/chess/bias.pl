20908943d96d98e7fbea29eec17b99070f0a387e
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-01-27 14:53:37 -0500

    Incorporate fork changes

--- examples/chess/bias.pl
+++ examples/chess/bias.pl
@@ -1,6 +1,6 @@
 max_clauses(1).
-max_vars(6).
-max_body(4).
+max_vars(8).
+max_body(7).
 
 % enable_pi.
 
@@ -10,30 +10,40 @@ direction(f, (in, in, in)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-body_pred(make_move, 4).
-body_pred(attacks, 3).
-body_pred(different_pos, 2).
+%body_pred(make_move, 4).
+%body_pred(attacks, 3).
+%body_pred(different_pos, 2).
 
 %body_pred(fork,3).
 
 %type(fork, (list, element, element)).
-type(make_move, (element, element, list, list)).
-type(attacks, (element, element, list)).
-type(different_pos, (element, element)).
+%type(make_move, (element, element, list, list)).
+%type(attacks, (element, element, list)).
+%type(different_pos, (element, element)).
 
-direction(make_move, (in, in, in, out)).
-direction(attacks, (in, out, out)).
-direction(different_pos, (out, out)).
+%direction(make_move, (in, in, in, out)).
+%direction(attacks, (in, out, out)).
+%direction(different_pos, (out, out)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %body_pred(pin, 3).
 
-%body_pred(behind, 4).
-%body_pred(pieceAt, 4).
-%body_pred(other_side, 2).
+body_pred(make_move, 4).
+body_pred(different_pos, 2).
+body_pred(behind, 4).
+body_pred(pieceAt, 4).
+body_pred(other_side, 2).
 
 %type(pin, (list, element, element)).
-%type(behind, (element, element, element, list)).
-%type(pieceAt, (element, list, element, element)).
-%type(other_side, (element, element)).
+type(make_move, (element, element, list, list)).
+type(different_pos, (element, element)).
+type(behind, (element, element, element, list)).
+type(pieceAt, (element, list, element, element)).
+type(other_side, (element, element)).
+
+direction(make_move, (in, in, in, out)).
+direction(different_pos, (out, out)).
+direction(behind, (in, out, out, in)).
+direction(pieceAt, (in, in, in, out)).
+direction(other_side, (in, in)).
--- examples/chess/exs.pl
+++ examples/chess/exs.pl
@@ -1,27 +1,12 @@
-pos(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 1, 1), contents(white, knight, 6, 4)], f4, d5)).
-pos(f([contents(black, king, 1, 8), contents(black, queen, 3, 8), contents(white, king, 5, 1), contents(white, knight, 1, 4)], a4, b6)).
-pos(f([contents(black, king, 1, 8), contents(black, queen, 4, 7), contents(white, king, 5, 1), contents(white, knight, 4, 5)], d5, b6)).
-pos(f([contents(black, king, 7, 5), contents(black, queen, 6, 6), contents(white, king, 2, 4), contents(white, knight, 3, 5)], c5, e4)).
-neg(f([contents(black, king, 1, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
-neg(f([contents(black, king, 2, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
-neg(f([contents(black, king, 3, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
-neg(f([contents(black, king, 4, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
-neg(f([contents(black, king, 5, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
-neg(f([contents(black, king, 6, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 2)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 3)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 4)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 5)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 6)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 7)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 8)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 7, 1)], e1, d2)).
-neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 7, 2)], e1, d2)).
-neg(f([contents(white, king, 5, 1), contents(white, rook, 2, 4), contents(black, knight, 6, 6), contents(black, king, 5, 8)], f6, e4)).
-neg(f([contents(white, king, 5, 1), contents(white, knight, 1, 3), contents(black, rook, 4, 6), contents(black, king, 5, 8)], a3, b5)).
-neg(f([contents(white, king, 5, 1), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
-neg(f([contents(white, king, 5, 2), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
-neg(f([contents(white, king, 5, 3), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
-neg(f([contents(white, king, 5, 4), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
-neg(f([contents(white, king, 7, 3), contents(white, pawn, 7, 4), contents(black, pawn, 2, 5), contents(black, pawn, 3, 6), contents(white, rook, 7, 7), contents(black, king, 2, 8)], g4, g5)).
+pos(f([contents(white, knight, 3, 1), contents(white, king, 5, 1), contents(black, rook, 1, 8), contents(black, king, 5, 8)], a8, a1)).
+pos(f([contents(white, king, 5, 2), contents(white, queen, 5, 4), contents(black, knight, 3, 5), contents(black, king, 2, 6)], e4, d4)).
+pos(f([contents(white, king, 2, 2), contents(white, rook, 3, 3), contents(black, king, 5, 5), contents(black, bishop, 1, 7)], a7, d4)).
+pos(f([contents(white, king, 4, 1), contents(white, rook, 6, 1), contents(black, pawn, 1, 7), contents(black, pawn, 2, 7), contents(black, pawn, 3, 7), contents(black, king, 2, 8), contents(black, rook, 5, 8)], f1, f8)).
+pos(f([contents(white, king, 2, 1), contents(white, queen, 6, 1), contents(black, knight, 3, 6), contents(black, king, 7, 6), contents(black, rook, 3, 8)], f1, a6)).
+pos(f([contents(white, king, 2, 1), contents(white, queen, 6, 1), contents(black, knight, 3, 6), contents(black, king, 7, 6), contents(black, rook, 3, 8)], f1, c4)).
+pos(f([contents(white, king, 2, 1), contents(white, queen, 6, 1), contents(black, knight, 3, 6), contents(black, king, 7, 6), contents(black, rook, 3, 8)], f1, c1)).
+neg(f([contents(white, king, 4, 1), contents(white, rook, 6, 1), contents(black, pawn, 1, 7), contents(black, pawn, 2, 7), contents(black, pawn, 3, 7), contents(black, king, 2, 8), contents(black, rook, 5, 8)], e8, e1)).
+neg(f([contents(white, king, 5, 1), contents(white, knight, 4, 2), contents(black, king, 4, 6), contents(black, rook, 6, 6)], d2, d4)).
+neg(f([contents(white, rook, 5, 6), contents(white, king, 6, 7), contents(black, pawn, 7, 7), contents(black, pawn, 8, 7), contents(black, king, 8, 8)], e6, e8)).
+neg(f([contents(white, knight, 7, 1), contents(white, pawn, 5, 2), contents(black, pawn, 5, 4), contents(black, bishop, 6, 4), contents(white, king, 7, 4), contents(black, king, 4, 5), contents(black, pawn, 7, 5)], d5, d4)).
+neg(f([contents(white, queen, 4, 1), contents(white, king, 5, 1), contents(black, king, 5, 8)], d1, d8)).
