74d3eb0a2b1d4fa7fd8c426f8362fc7d191b117c
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-02-21 17:09:35 -0500

    Merge fork + pin examples

--- examples/chess/exs.pl
+++ examples/chess/exs.pl
@@ -12,3 +12,31 @@ neg(f([contents(white, knight, 7, 1), contents(white, pawn, 5, 2), contents(blac
 neg(f([contents(white, queen, 4, 1), contents(white, king, 5, 1), contents(black, king, 5, 8)], d1, d8)).
 neg(f([contents(white, knight, 5, 1), contents(white, king, 1, 5), contents(black, king, 5, 8), contents(black, bishop, 8, 8)], h8, c3)).
 neg(f([contents(white, knight, 4, 2), contents(white, king, 1, 5), contents(black, king, 5, 8), contents(black, bishop, 8, 8)], h8, c3)).
+
+pos(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 1, 1), contents(white, knight, 6, 4)], f4, d5)).
+pos(f([contents(black, king, 1, 8), contents(black, queen, 3, 8), contents(white, king, 5, 1), contents(white, knight, 1, 4)], a4, b6)).
+pos(f([contents(black, king, 1, 8), contents(black, queen, 4, 7), contents(white, king, 5, 1), contents(white, knight, 4, 5)], d5, b6)).
+pos(f([contents(black, king, 7, 5), contents(black, queen, 6, 6), contents(white, king, 2, 4), contents(white, knight, 3, 5)], c5, e4)).
+neg(f([contents(black, king, 1, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
+neg(f([contents(black, king, 2, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
+neg(f([contents(black, king, 3, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
+neg(f([contents(black, king, 4, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
+neg(f([contents(black, king, 5, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
+neg(f([contents(black, king, 6, 8), contents(white, king, 5, 1), contents(white, knight, 3, 4)], c4, b6)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 1)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 2)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 3)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 4)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 5)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 6)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 7)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 6, 8)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 7, 1)], e1, d2)).
+neg(f([contents(black, king, 5, 7), contents(black, rook, 3, 7), contents(white, king, 5, 1), contents(white, knight, 7, 2)], e1, d2)).
+neg(f([contents(white, king, 5, 1), contents(white, rook, 2, 4), contents(black, knight, 6, 6), contents(black, king, 5, 8)], f6, e4)).
+neg(f([contents(white, king, 5, 1), contents(white, knight, 1, 3), contents(black, rook, 4, 6), contents(black, king, 5, 8)], a3, b5)).
+neg(f([contents(white, king, 5, 1), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
+neg(f([contents(white, king, 5, 2), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
+neg(f([contents(white, king, 5, 3), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
+neg(f([contents(white, king, 5, 4), contents(black, queen, 4, 8), contents(black, king, 5, 8)], d8, d3)).
+neg(f([contents(white, king, 7, 3), contents(white, pawn, 7, 4), contents(black, pawn, 2, 5), contents(black, pawn, 3, 6), contents(white, rook, 7, 7), contents(black, king, 2, 8)], g4, g5)).
