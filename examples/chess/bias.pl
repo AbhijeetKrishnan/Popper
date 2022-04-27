f5c772c5a8e7c033db1e00003fe4bce12d158874
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-04-27 16:40:13 -0700

    Reorder predicates

--- examples/chess/bias.pl
+++ examples/chess/bias.pl
@@ -5,43 +5,50 @@ max_body(5).
 % enable_pi.
 
 head_pred(f,3).
-type(f, (list, element, element)).
-direction(f, (in, in, in)).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%body_pred(make_move, 4).
 body_pred(attacks, 3).
-%body_pred(different_pos, 2).
-
-%body_pred(fork,3).
-
-%type(fork, (list, element, element)).
-%type(make_move, (element, element, list, list)).
-type(attacks, (element, element, list)).
-%type(different_pos, (element, element)).
-
-%direction(make_move, (in, in, in, out)).
-direction(attacks, (in, out, out)).
-%direction(different_pos, (out, out)).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%body_pred(pin, 3).
-
 body_pred(make_move, 4).
 body_pred(different_pos, 2).
 body_pred(behind, 4).
 body_pred(pieceAt, 4).
 body_pred(other_side, 2).
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%body_pred(make_move, 4).
+
+%body_pred(different_pos, 2).
+
+%body_pred(fork,3).
+
+%type(fork, (list, element, element)).
+%type(make_move, (element, element, list, list)).
+
+%type(different_pos, (element, element)).
+
+%direction(make_move, (in, in, in, out)).
+
+%direction(different_pos, (out, out)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%body_pred(pin, 3).
+
+
+
 %type(pin, (list, element, element)).
+type(f, (list, element, element)).
+type(attacks, (element, element, list)).
 type(make_move, (element, element, list, list)).
 type(different_pos, (element, element)).
 type(behind, (element, element, element, list)).
 type(pieceAt, (element, list, element, element)).
 type(other_side, (element, element)).
 
+direction(f, (in, in, in)).
+direction(attacks, (in, out, out)).
 direction(make_move, (in, in, in, out)).
 direction(different_pos, (out, out)).
 direction(behind, (in, out, out, in)).
