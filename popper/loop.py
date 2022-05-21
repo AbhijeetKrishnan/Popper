fb4ea76a65671a3fe591d0596dafd62d45072d77
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-05-21 02:22:17 -0400

    Increase buffer limit

--- popper/loop.py
+++ popper/loop.py
@@ -135,7 +135,7 @@ def popper(settings, stats):
     constrainer = Constrain()
     constraint_rule_buffer = []
     valid_tactics = set()
-    BUFFER_LIMIT = 1 # arbitrary value
+    BUFFER_LIMIT = 1000 # arbitrary value
 
     for size in range(1, settings.max_literals + 1):
         stats.update_num_literals(size)
